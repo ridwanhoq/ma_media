67c9fa9892e0780b7eacace9b80099c943cab8d8
ridwanul <ridwanul.riseuplabs@gmail.com>
2022-12-01 14:01:48 +0600

    Modified: basic search

--- resources/views/admin/searches/basic_search.blade.php
+++ resources/views/admin/searches/basic_search.blade.php
@@ -1,118 +1,151 @@
 @extends('admin.layouts.app')
 
-    @section('styles')
+@section('styles')
     <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/core/menu/menu-types/vertical') }}-menu.css">
     <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/core/colors/palette-gra') }}dient.css">
-    @endsection
+@endsection
 
-    
-    <!-- BEGIN: Content-->
-    <div class="app-content content">
-        <div class="content-overlay"></div>
-        <div class="content-wrapper">
-            <div class="content-header row">
-                <div class="content-header-left col-md-6 col-12 mb-2">
-                    <h3 class="content-header-title">Basic Forms</h3>
-                    <div class="row breadcrumbs-top">
-                        <div class="breadcrumb-wrapper col-12">
-                            <ol class="breadcrumb">
-                                <li class="breadcrumb-item"><a href="index.html">Home</a>
-                                </li>
-                                <li class="breadcrumb-item"><a href="#">Form Layouts</a>
-                                </li>
-                                <li class="breadcrumb-item active">Basic Forms
-                                </li>
-                            </ol>
-                        </div>
-                    </div>
-                </div>
-                <div class="content-header-right col-md-6 col-12">
-                    <div class="btn-group float-md-right" role="group" aria-label="Button group with nested dropdown">
-                        <button class="btn btn-info round dropdown-toggle dropdown-menu-right box-shadow-2 px-2 mb-1" id="btnGroupDrop1" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="ft-settings icon-left"></i> Settings</button>
-                        <div class="dropdown-menu" aria-labelledby="btnGroupDrop1"><a class="dropdown-item" href="card-bootstrap.html">Cards</a><a class="dropdown-item" href="component-buttons-extended.html">Buttons</a></div>
+
+<!-- BEGIN: Content-->
+<div class="app-content content">
+    <div class="content-overlay"></div>
+    <div class="content-wrapper">
+        <div class="content-header row">
+            <div class="content-header-left col-md-6 col-12 mb-2">
+                <h3 class="content-header-title">Basic Forms</h3>
+                <div class="row breadcrumbs-top">
+                    <div class="breadcrumb-wrapper col-12">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="index.html">Home</a>
+                            </li>
+                            <li class="breadcrumb-item"><a href="#">Form Layouts</a>
+                            </li>
+                            <li class="breadcrumb-item active">Basic Forms
+                            </li>
+                        </ol>
                     </div>
                 </div>
             </div>
-            <div class="content-body">
-                <!-- Basic form layout section start -->
-                <section id="basic-form-layouts">
-                    <div class="row match-height">
+            <div class="content-header-right col-md-6 col-12">
+                <div class="btn-group float-md-right" role="group" aria-label="Button group with nested dropdown">
+                    <button class="btn btn-info round dropdown-toggle dropdown-menu-right box-shadow-2 px-2 mb-1"
+                        id="btnGroupDrop1" type="button" data-toggle="dropdown" aria-haspopup="true"
+                        aria-expanded="false"><i class="ft-settings icon-left"></i> Settings</button>
+                    <div class="dropdown-menu" aria-labelledby="btnGroupDrop1"><a class="dropdown-item"
+                            href="card-bootstrap.html">Cards</a><a class="dropdown-item"
+                            href="component-buttons-extended.html">Buttons</a></div>
+                </div>
+            </div>
+        </div>
+        <div class="content-body">
+            <!-- Basic form layout section start -->
+            <section id="basic-form-layouts">
+                <div class="row match-height">
 
-                        <div class="col-md-6">
-                            <div class="card">
-                                <div class="card-header">
-                                    <h4 class="card-title" id="basic-layout-form">Project Info</h4>
-                                    <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
-                                    <div class="heading-elements">
-                                        <ul class="list-inline mb-0">
-                                            <li><a data-action="collapse"><i class="ft-minus"></i></a></li>
-                                            <li><a data-action="reload"><i class="ft-rotate-cw"></i></a></li>
-                                            <li><a data-action="expand"><i class="ft-maximize"></i></a></li>
-                                            <li><a data-action="close"><i class="ft-x"></i></a></li>
-                                        </ul>
-                                    </div>
+                    <div class="col-md-6">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4 class="card-title" id="basic-layout-form">Project Info</h4>
+                                <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
+                                <div class="heading-elements">
+                                    <ul class="list-inline mb-0">
+                                        <li><a data-action="collapse"><i class="ft-minus"></i></a></li>
+                                        <li><a data-action="reload"><i class="ft-rotate-cw"></i></a></li>
+                                        <li><a data-action="expand"><i class="ft-maximize"></i></a></li>
+                                        <li><a data-action="close"><i class="ft-x"></i></a></li>
+                                    </ul>
                                 </div>
-                                <div class="card-content collapse show">
-                                    <div class="card-body">
-                                        <div class="card-text">
-                                            <p>Basic Search Profiles</p>
-                                        </div>
-                                        <form class="form">
-                                            <div class="form-body">
-                                                
-                                                <h4 class="form-section"><i class="ft-user"></i> Personal Info</h4>
-                                                
-                                                <div class="row">
-                                                    <div class="col-md-6">
-                                                        <div class="form-group">
-                                                            <label for="projectinput5">Gender</label>
-                                                            {!! Form::select('gender', $genders_array, request()->gender ?? null, ['class' => 'form-control', 'placeholder' => __('--Select Any--')]) !!}
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-md-6">
-                                                        <div class="form-group">
-                                                            <label for="projectinput5">Age Range</label>
-                                                            {!! Form::select('gender', $genders_array, request()->gender ?? null, ['class' => 'form-control', 'placeholder' => __('--Select Any--')]) !!}
-                                                        </div>
+                            </div>
+                            <div class="card-content collapse show">
+                                <div class="card-body">
+                                    <div class="card-text">
+                                        <p>Basic Search Profiles</p>
+                                    </div>
+                                    <form class="form">
+                                        <div class="form-body">
+
+                                            <h4 class="form-section"><i class="ft-user"></i> Personal Info</h4>
+
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="projectinput5">{{ __("Gender") }}</label>
+                                                        {!! Form::select('gender', $genders_array, request()->gender ?? null, [
+                                                            'class' => 'form-control',
+                                                            'placeholder' => __('--Select Any--'),
+                                                        ]) !!}
                                                     </div>
                                                 </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="projectinput5">{{ __("Age Range") }}</label>
+                                                        {!! Form::select('gender', $genders_array, request()->gender ?? null, [
+                                                            'class' => 'form-control',
+                                                            'placeholder' => __('--Select Any--'),
+                                                        ]) !!}
+                                                    </div>
+                                                </div>
+                                            </div>
 
 
-                                                <h4 class="form-section"><i class="ft-user"></i> Personal Info</h4>
+                                            <h4 class="form-section"><i class="ft-user"></i> Personal Info</h4>
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="projectinput1">Religion</label>
+                                                        {!! Form::select('religion', $religions_array, request()->religion ?? null, [
+                                                            'class' => 'form-control',
+                                                            'placeholder' => __('--Select Any--'),
+                                                        ]) !!}
+                                                    </div>
+                                                </div>
                                                 <div class="row">
                                                     <div class="col-md-6">
                                                         <div class="form-group">
-                                                            <label for="projectinput1">Religion</label>
-                                                            {!! Form::select('religion', $religions_array, request()->religion ?? null, ['class' => 'form-control', 'placeholder' => __('--Select Any--')]) !!}
+                                                            <label for="religion">Religion</label>
+                                                            {!! Form::select('religion', $religions_array, request()->religion ?? null, [
+                                                                'class' => 'form-control',
+                                                                'placeholder' => __('--Select Any--'),
+                                                            ]) !!}
                                                         </div>
                                                     </div>
                                                     <div class="col-md-6">
                                                         <div class="form-group">
-                                                            <label for="projectinput2">Last Name</label>
-                                                            <input type="text" id="projectinput2" class="form-control" placeholder="Last Name" name="lname">
+                                                            <label for="division_id">Division</label>
+                                                            {!! Form::select('division_id', $divisions_array, request()->division_id ?? null, [
+                                                                'class' => 'form-control',
+                                                                'placeholder' => __('--Select Any--'),
+                                                            ]) !!}
                                                         </div>
                                                     </div>
+                                                    
                                                 </div>
                                                 <div class="row">
                                                     <div class="col-md-6">
                                                         <div class="form-group">
                                                             <label for="projectinput3">E-mail</label>
-                                                            <input type="text" id="projectinput3" class="form-control" placeholder="E-mail" name="email">
+                                                            <input type="text" id="projectinput3"
+                                                                class="form-control" placeholder="E-mail"
+                                                                name="email">
                                                         </div>
                                                     </div>
                                                     <div class="col-md-6">
                                                         <div class="form-group">
                                                             <label for="projectinput4">Contact Number</label>
-                                                            <input type="text" id="projectinput4" class="form-control" placeholder="Phone" name="phone">
+                                                            <input type="text" id="projectinput4"
+                                                                class="form-control" placeholder="Phone"
+                                                                name="phone">
                                                         </div>
                                                     </div>
                                                 </div>
 
-                                                <h4 class="form-section"><i class="la la-paperclip"></i> Requirements</h4>
+                                                <h4 class="form-section"><i class="la la-paperclip"></i> Requirements
+                                                </h4>
 
                                                 <div class="form-group">
                                                     <label for="companyName">Company</label>
-                                                    <input type="text" id="companyName" class="form-control" placeholder="Company Name" name="company">
+                                                    <input type="text" id="companyName" class="form-control"
+                                                        placeholder="Company Name" name="company">
                                                 </div>
 
 
@@ -138,17 +171,17 @@
                                                     <i class="la la-check-square-o"></i> Save
                                                 </button>
                                             </div>
-                                        </form>
-                                    </div>
+                                    </form>
                                 </div>
                             </div>
                         </div>
-
                     </div>
 
-                </section>
-                <!-- // Basic form layout section end -->
-            </div>
+                </div>
+
+            </section>
+            <!-- // Basic form layout section end -->
         </div>
     </div>
-    <!-- END: Content-->
+</div>
+<!-- END: Content-->
